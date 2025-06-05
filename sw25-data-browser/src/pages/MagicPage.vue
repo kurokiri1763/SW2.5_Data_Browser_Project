1d4b5b4294579b6284e9cdf8433f952062b6de00
kurokiri1763 <kurokiri1763@gmail.com>
2025-06-05 13:23:51 +0900

    魔法追加ページのリンク追加

--- sw25-data-browser/src/pages/MagicPage.vue
+++ sw25-data-browser/src/pages/MagicPage.vue
@@ -1,8 +1,14 @@
+<script lang="ts">
+import { RouterLink } from 'vue-router';
+import MagicForm from '../components/magic/MagicForm.vue';
+</script>
+
 <template>
   <section class="section">
     <div class="container">
       <h1 class="title">魔法データページ</h1>
-
+        <RouterLink to="/addmagicform" class="button">魔法データ新規追加</h2>
+          <MagicForm />
       <div class="buttons">
         <RouterLink to="/" class="button is-link" >メインメニューに戻る</RouterLink>
       </div>
