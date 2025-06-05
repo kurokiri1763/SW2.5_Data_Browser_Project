8eb58251a61119e5c33231911415bb3a0e9d6cd0
kurokiri1763 <kurokiri1763@gmail.com>
2025-06-05 09:05:42 +0900

    各カテゴリのフォーム実装

---
+++ sw25-data-browser/src/components/magic/MagicForm.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+  import { ref } from 'vue'
+  import MagicNameInput from './fields/MagicNameInput.vue'
+  import MagicSystemSelect from './fields/MagicSystemSelect.vue'
+  import MagicMPInput from './fields/MagicMPInput.vue'
+  import MagicEffectTextarea from './fields/MagicEffectTextarea.vue'
+  
+  const magic = ref({
+    name: '',
+    system: '神聖魔法',
+    mp: 0,
+    effect: '',
+  })
+  
+  const submit = () => {
+    console.log('保存:', magic.value)
+    // データ保存ロジックを後で追加
+  }
+  </script>
+
+<template>
+    <form @submit.prevent="submit">
+      <MagicNameInput v-model="magic.name" />
+      <MagicSystemSelect v-model="magic.system" />
+      <MagicMPInput v-model.number="magic.mp" />
+      <MagicEffectTextarea v-model="magic.effect" />
+  
+      <div class="field">
+        <button class="button is-primary" type="submit">保存</button>
+      </div>
+    </form>
+  </template>
---
+++ sw25-data-browser/src/components/magic/fields/MagicNameInput.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="field">
+      <label class="label">魔法名</label>
+      <div class="control">
+        <input class="input" type="text" :value="modelValue" @input="$emit('update:modelValue', $event.target.value)" required />
+      </div>
+    </div>
+  </template>
+  
+  <script setup lang="ts">
+  defineProps<{ modelValue: string }>()
+  defineEmits(['update:modelValue'])
+  </script>
